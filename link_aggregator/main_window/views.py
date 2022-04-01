9848b5b5e12f0407747a09a8f760c5d53dc53465
Bulad <tbulad222@gmail.com>
2022-04-01 11:11:30 +0300

    Окно регистрации и входа

--- link_aggregator/main_window/views.py
+++ link_aggregator/main_window/views.py
@@ -3,3 +3,11 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'main_window/index.html')
+
+
+def registrations(request):
+    return render(request, 'main_window/registrations.html')
+
+
+def authorization(request):
+    return render(request, 'main_window/authorization.html')
