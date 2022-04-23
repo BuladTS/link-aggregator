3d97ae6de1b0e1e14fa0800071c73b2559c82815
Bulad <tbulad222@gmail.com>
2022-04-23 13:16:09 +0300

    Возможность смотреть тэги

--- link_aggregator/main_window/static/js/table_links.js
+++ link_aggregator/main_window/static/js/table_links.js
@@ -1,19 +1,62 @@
 const tableLinks = document.querySelectorAll('[data-links]');
-const allDescriptions = document.querySelectorAll('[data-content]')
+const allDescriptions = document.querySelectorAll('[data-content-description]');
+const buttons = document.querySelectorAll('[data-pressed-button]');
+const allTags = document.querySelectorAll('[data-content-tag]');
 
+allDescriptions[0].classList.toggle('hidden');
 
-tableLinks.forEach(function (item) {
+buttons.forEach(function (item, i) {
     item.addEventListener('click', function () {
-
-        const description = document.querySelector('#' + this.dataset.link);
-        
-        allDescriptions.forEach(function (item) {
-            item.classList.add('hidden');
+        buttons.forEach(function (item){
+            item.classList.remove('pressed_button');
         })
-        description.classList.toggle('hidden');
+        if (i === 0) {
+            buttons[0].classList.toggle('pressed_button');
+            allTags.forEach(function (item) {
+                if (!item.classList.contains('hidden')) {
+                     item.classList.add('hidden');
+                     const description_id = item.getAttribute('data-description');
+                     const description = document.querySelector('#' + description_id);
+                     description.classList.toggle('hidden');
+                 }
+            })
+        }
+        else {
+            buttons[1].classList.toggle('pressed_button');
+            allDescriptions.forEach(function (item) {
+                 if (!item.classList.contains('hidden')) {
+                     item.classList.add('hidden');
+                     const tag_id = item.getAttribute('data-tag');
+                     const tag = document.querySelector('#' + tag_id);
+                     tag.classList.toggle('hidden');
+                 }
+            })
+
+        }
+
     })
 })
 
+tableLinks.forEach(function (item) {
+    item.addEventListener('click', function () {
+        if (buttons[0].classList.contains('pressed_button')) {
+             const description = document.querySelector('#description-' + this.dataset.link);
+             allDescriptions.forEach(function (item) {
+                 item.classList.add('hidden');
+             })
+             description.classList.toggle('hidden');
+        }
+        else {
+            const tag = document.querySelector('#tag-' + this.dataset.link);
+            allTags.forEach(function (item) {
+                item.classList.add('hidden');
+            })
+            tag.classList.toggle('hidden');
+        }
 
-console.log(tableLinks);
+
+
+    })
+})
+console.log(allTags);
 console.log(allDescriptions);
